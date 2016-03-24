92b2d90e979c728e9cabf3139ec6425ca5126e34
lzmkhan <lzmkhan@yahoo.com>
2016-03-25 00:31:16 +0530

    GCM and otp
    Added GCM and OTP integration.

--- ArihantDistributors/src/com/example/arihantdistributors/OTPHandler.java
+++ ArihantDistributors/src/com/example/arihantdistributors/OTPHandler.java
@@ -7,6 +7,10 @@ import java.net.URLConnection;
 import java.net.URLEncoder;
 import java.util.*;
 
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 import android.content.Context;
 import android.os.AsyncTask;
 import android.util.Log;
@@ -26,24 +30,17 @@ public class OTPHandler {
 		OTPHolder = new String(OTP);
 	}
 	
-	public boolean sendOTP(Context c)
+	public boolean sendOTP(Context c, int mode)
 	{
 		boolean status = false;
-		
-		WebserviceHandler wbx = new WebserviceHandler(9);
-		wbx.set_context(c);
-		wbx.query1 = Info_Store.getInstance().Mobile;
-		wbx.query2 = OTPHolder;
-		wbx.execute();
-		
-		
-		
-		
-		
-		//MyPost mp = new MyPost();
-		//mp.execute();
-		
-		//status = sendOTPtoSMS(Info_Store.getInstance().AccountID, OTPHolder);
+	
+		MyPostByPHP mp = new MyPostByPHP();
+		if(mode==1)
+		{
+			mp.mode=1;
+		}
+				mp.execute();
+
 		return status;
 	}
 	
@@ -66,13 +63,15 @@ public class OTPHandler {
 	     //Your authentication key
 	       String authkey = "";
 	       //Multiple mobiles numbers separated by comma
+	       if(Info_Store.getInstance().Mobile.equals("") || Info_Store.getInstance().Mobile.equals("None"))
+	    	   Info_Store.getInstance().Mobile = TempData.getInstance().number;
 	       String mobiles = "91" + Info_Store.getInstance().Mobile;
 	       //Sender ID,While using route4 sender id should be 6 characters long.
-	       String senderId = "123456";
+	       String senderId = "MARCUS";
 	       //Your message to send, Add URL encoding here.
 	       String message = "Your OTP is " + OTPHolder;
 	       //define route
-	       String route="default";
+	       String route="4";
 
 	       URLConnection myURLConnection=null;
 	       URL myURL=null;
@@ -121,4 +120,74 @@ public class OTPHandler {
 	    }
 	}
 	
+	
+	private class MyPostByPHP extends AsyncTask<Void, Void, Void> 
+	{
+		
+		int mode =0;
+	    @SuppressWarnings("deprecation")
+		@Override
+	    protected Void doInBackground(Void... arg0) 
+	    {
+	      
+	       if(Info_Store.getInstance().Mobile.equals("") || Info_Store.getInstance().Mobile.equals("None"))
+	    	   Info_Store.getInstance().Mobile = TempData.getInstance().number;
+	       String mobiles =  Info_Store.getInstance().Mobile;
+	       //Sender ID,While using route4 sender id should be 6 characters long.
+	    
+	       //Your message to send, Add URL encoding here.
+	       String message = "Your OTP is " + OTPHolder;
+
+
+	       URLConnection myURLConnection=null;
+	       URL myURL=null;
+	       BufferedReader reader=null;
+
+	       //encoding message 
+	       String encoded_message=URLEncoder.encode(message);
+	       String mainUrl;
+	       //Send SMS API
+	       if(mode==0)
+	       {
+	    	   mainUrl ="http://autestdomain.comule.com/Arihantwebservices/otp.php?";
+	       }
+	       else
+	       {
+	    	   mainUrl ="http://autestdomain.comule.com/Arihantwebservices/otp_sign.php?";
+	       }
+	       //Prepare parameter string 
+	       StringBuilder sbPostData= new StringBuilder(mainUrl);
+	        
+	       sbPostData.append("phone="+mobiles);
+	       sbPostData.append("&otp="+encoded_message);
+	       
+	       //final string
+	       mainUrl = sbPostData.toString();
+	       try
+	       {
+	           //prepare connection
+	           myURL = new URL(mainUrl);
+	           myURLConnection = myURL.openConnection();
+	           myURLConnection.connect();
+	           reader= new BufferedReader(new InputStreamReader(myURLConnection.getInputStream()));
+	           
+	           //reading response 
+	           String response;
+	           while ((response = reader.readLine()) != null) 
+	           //print response 
+	           Log.d("RESPONSE", ""+response);
+	           
+	           
+	           //finally close connection
+	           reader.close();
+	       } 
+	       catch (IOException e) 
+	       { 
+	       	e.printStackTrace();
+	       }
+		return null; 
+
+	    }
+	}
+
 }
